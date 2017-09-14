300f6799bb4bbdd3ee3af6553d3cea4c8265199c
Martin Cubaa <martin.cuba459@gmail.com>
2017-09-13 23:00:40 -0300

    wachin arregla todo
    pero es marica

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://localhost/HProfesional/';
+$config['base_url'] = "http://".$_SERVER['SERVER_NAME'].str_replace("index.php", "",  $_SERVER['SCRIPT_NAME']);
 
 /*
 |--------------------------------------------------------------------------
