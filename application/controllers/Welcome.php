9445c7f432d5b256033baedc5e400cbeebfc56f0
Martin Cubaa <martin.cuba459@gmail.com>
2017-09-15 11:14:43 -0300

    ion auth

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -2,7 +2,15 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
-
+	function __construct(){
+		parent::__construct(); /*Ejecuta el constructor del padre*/
+		//$this->load->helper('mihelper'); // Primero busca en helper de Application, sino va a System.
+		$this->load->database();
+		$this->load->library('grocery_crud');
+		$this->load->helper('url');
+		$this->load->helper('form'); // Viene por defecto con CI. Crear formularios con ese helper.
+		$this->load->model('nadador_model');
+	}
 	/**
 	 * Index Page for this controller.
 	 *
@@ -20,6 +28,24 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		//$this->load->view('inicio.php');
+		$crud = new grocery_CRUD();
+		$crud->set_table('nadador');
+		$crud->columns('DNI', 'Apellido');
+		$crud->fields('Apellido');
+		//$crud->unset_delete();
+		//$crud->unset_read();
+		//$crud->unset_edit();
+		//$crud->unset_export();
+		//$crud->unset_print();
+		//$crud->unset_add();
+		$crud->unset_operations();
+
+
+
+		/* Generamos la tabla */
+    	$output = $crud->render();
+    	$this->load->view('headers', $output);
+    	$this->load->view('inicio', $output);
 	}
 }
